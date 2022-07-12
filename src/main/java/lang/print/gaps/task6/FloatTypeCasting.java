728f0b450735478ebf147b2bc0fa648931f3f0cc
tikmart <t.martirosyan012@gmail.com>
2022-07-12 17:51:01 +0400

    Task 6 first commit

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+
+        numberToBeRounded = 19.4F;
+
+        numberToBeRounded = Math.round(numberToBeRounded);
+
+        System.out.println(numberToBeRounded);
+
     }
 }
